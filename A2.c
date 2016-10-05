d5ca7d52490cf99c4a12669a90e7a643acd64d86
Jessica <jauthier95@gmail.com>
2016-10-05 18:47:01 -0400

    back

--- A2.c
+++ A2.c
@@ -5,12 +5,18 @@
 int main(int argc, char* argv[]){
 	
 	int len;
+	char * temp;
 	if (argc == 2){
-		len = strtol(argv[1]);
+		len = strtol(argv[1], &temp, 10);
 	} else {
 		printf("You must enter the length of the binary string.\n");
 		exit(0);
 	}
+
+	printf("%d\n", len);
+
+
+	return 0;
 }
 
 
