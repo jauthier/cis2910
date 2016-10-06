171cf0ab94bc68955506976d6cb0aa40110435cf
jauthier <jauthier@mail.uoguelph.ca>
2016-10-05 20:29:57 -0400

    a2

--- A2.c
+++ A2.c
@@ -5,7 +5,7 @@
 int main(int argc, char* argv[]){
 	
 	int len;
-	char * temp;
+	char * temp, str;
 	if (argc == 2){
 		len = strtol(argv[1], &temp, 10);
 	} else {
@@ -13,10 +13,21 @@ int main(int argc, char* argv[]){
 		exit(0);
 	}
 
+
 	printf("%d\n", len);
 
+	binString(str, len);
 
 	return 0;
 }
 
+void binString(string str, int lenLeft){
+
+	if (lenLeft == 0){
+		printf("%s\n", str);
+	} else {
+		binString(strcat("0",str), lenLeft - 1);
+		binString(strcat("1",str), lenLeft - 1);
+	}
+}
 
