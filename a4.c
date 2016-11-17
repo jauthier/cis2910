4bc0b1229eadc56c5a4cf5073dbfc5818ff011e0
jauthier <jauthier@mail.uoguelph.ca>
2016-11-16 19:37:19 -0500

    go

--- a4.c
+++ a4.c
@@ -41,7 +41,7 @@ int main(int argc, char *argv[]){
 			char row[vertices*2];
 			printf("here\n");
 			fgets(row,40,fp);
-			if (row == NULL)
+			if (strcmp(row, "\n") == 0)
 				printf("null\n");
 printf("1 %s\n", row);
 			char *token = strtok(row," ");
